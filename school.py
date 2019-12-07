fd3421b9bb6b6e85a9f56477a23255d127d0d87e
mokome <mokome@gmail.com>
2019-12-07 00:32:44 -0500

    Completed Object Oriented Attributes with Functions

--- school.py
+++ school.py
@@ -1,1 +1,19 @@
 class School:
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+        
+    def add_student(self, name, grade_level):
+        if self.roster.get(grade_level): self.roster[grade_level].append(name)
+        else: self.roster[grade_level] = [name]
+        # self.roster[grade_level] = self.roster.get(grade_level, [name]).append(name)
+        
+    def grade(self, grade_level):
+        return self.roster.get(grade_level, [])
+    
+    def sort_roster(self):
+        for g in self.roster:
+            self.roster[g].sort()
+        
+        return self.roster
+        
